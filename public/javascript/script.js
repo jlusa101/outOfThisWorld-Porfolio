350923594901a7a549ec1782185065523e2846b2
Joona Lusa <joona.lusa@hotmail.com>
2022-02-11 18:02:41 -0500

    mobile responsiveness added

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,18 +1,22 @@
-$(document).ready(function() {
-    $("#header").fadeIn(1000);
-});
+// $(document).ready(function() {
+//     $("#header").fadeIn(1000);
+// });
 
 $(document).ready(function() {
+    $("#header").fadeIn(1000);
+
     $(".menu").on('mouseover', 'a', function() {
         $(this).css({ "border-bottom": "solid 2px #E3963E" });
     }).on('mouseout', 'a', function() {
         $(this).css({ "border-bottom": "none" });
     })
-});
 
-$(document).ready(function() {
     $("#contact-link").click(function() {
         $("#header").css({ "display": "none" });
         $("#contact").slideDown("slow");
     });
 });
+
+// $(document).ready(function() {
+
+// });
