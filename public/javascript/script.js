4d9f84eef82fa73c24f3ca9f7d6b7b82961eaebe
Joona Lusa <joona.lusa@hotmail.com>
2022-02-12 10:56:44 -0500

    added an email me functionality to contact page

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -27,4 +27,38 @@ $(document).ready(function() {
         $("#work").css({ "display": "none" });
         $("#contact").slideDown("slow");
     });
+
 });
+
+var form = document.getElementById("my-form");
+
+// Handles form submissions and sends myself an email from the user 
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Thanks for your message!";
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "Oops! There was a problem sending your email";
+                }
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem sending your email";
+    });
+
+}
+form.addEventListener("submit", handleSubmit)
